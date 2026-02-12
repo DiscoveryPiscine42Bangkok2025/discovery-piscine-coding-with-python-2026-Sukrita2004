2d62a2b14029dca338ae69bff64d9c47b5fa075f
Sukrita2004 <Fame2047@gmail.com>
2026-02-13 00:34:06 +0700

    finish

--- Rush00/ex00/main.py
+++ Rush00/ex00/main.py
@@ -32,63 +32,63 @@ def main():
 #     print("@" * 20)
 
 
-    print("Test 4")
-    board2 = """\
-R...
-.K.....
-..P.
-....\
-"""
-    checkmate(board2)
-    print("@" * 20)
+#     print("Test 4")
+#     board2 = """\
+# R...
+# .K.....
+# ..P.
+# ....\
+# """
+#     checkmate(board2)
+#     print("@" * 20)
 
-    print("Test 5")
-    board2 = """\
-R...
-.K..
-..K.
-....\
-"""
-    checkmate(board2)
-    print("@" * 20)
+#     print("Test 5")
+#     board2 = """\
+# R...
+# .K..
+# ..K.
+# ....\
+# """
+#     checkmate(board2)
+#     print("@" * 20)
 
-    print("Test 6")
-    board2 = """\
-...
-R...
-....
-..P.
-....\
-"""
-    checkmate(board2)
-    print("@" * 20)
+#     print("Test 6")
+#     board2 = """\
+# ...
+# R...
+# ....
+# ..P.
+# ....\
+# """
+#     checkmate(board2)
+#     print("@" * 20)
 
-    #เพิ่มเติม
-    print("Test 7")
-    board2 = """\
-..K.
-..P.
-..Q.\
-"""
-    checkmate(board2)
-    print("@" * 20)
+#     #เพิ่มเติม
+#     print("Test 7")
+#     board2 = """\
+# ..K.
+# ..P.
+# ..Q.\
+# """
+#     checkmate(board2)
+#     print("@" * 20)
     
-    print("Test 8")
-    board3 = """\
-...
-.K.
-P.P\
-"""
-    checkmate(board3)
-    print("@" * 20)
+#     print("Test 8")
+#     board3 = """\
+# ...
+# .K.
+# P.P\
+# """
+#     checkmate(board3)
+#     print("@" * 20)
 
-    print("Test 9")
-    board4 = """\
-....
-.Q..
-....\
-"""
-    checkmate(board4)
+#     print("Test 9")
+#     board4 = """\
+# ....
+# .Q..
+# ....\
+# """
+#     checkmate(board4)
 
 if __name__ == "__main__":
     main()
